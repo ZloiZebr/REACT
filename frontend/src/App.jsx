45392f1a15bd196d292bb370880686f23cc24f48
Konstantin Nikittsev <nikitsev@yandex.ru>
2026-06-24 12:35:31 +0300

    HomeworkReact

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,15 @@
+import { useState } from "react"
+
 function App() {
-  return null
+  return( 
+    <div>
+      <h1>Домашнее задание по React №1</h1>
+      <button onClick= { () => alert("Hello World")
+      } >
+        Нажми меня 
+      </button>
+    </div>
+  )
 }
 
 export default App
