32ba6ef77e72a44243094b944f166099b41cb95f
Konstantin Nikittsev <nikitsev@yandex.ru>
2026-07-25 20:05:49 +0300

    HomeworlReact№6

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,52 +1,16 @@
-import TaskList from './components/UI/TaskList'
-import AddTask from './components/UI/AddTask'
-import { useState, useEffect } from 'react'
 import styles from './App.module.css'
-import axios from 'axios'
-
-  
-const API_URL = import.meta.env.VITE_API_URL
+import AddProduct1 from './components/UI/AddProduct1'
+import AddProduct2 from './components/UI/AddProduct2'
 
 function App() {
-  const [tasks, setTasks] = useState([])
-  const [tasksLoading, setTasksLoading] = useState(true)
-  const [tasksError, setTasksError] = useState(null)
-
-  const addTask = (newTask)=>{
-    
-    axios.post(`${API_URL}/tasks`,newTask)
-      .then(response => setTasks([...tasks,response.data]))
-  } 
-
-  useEffect(() => {
-          const getTasks = async() => { 
-              try {
-                  const response = await axios.get(`${API_URL}/tasks`)
-                  setTasks(response.data) 
-              }
-              catch(error) {
-                  console.error(error)
-                  setTasksLoading(false)
-                  setTasksError("Не удалось загрузить данные.")
-              }
-              finally {
-                  setTasksLoading(false)
-              }
-          }
-      getTasks()    
-      }, [])
-
   return(
     <div className={styles.container}>
       <main className={styles.main}>
-        <TaskList
-          tasks={tasks}
-          tasksLoading={tasksLoading}
-          tasksError={tasksError}
-        />
+        <h1>Домашнее задание по React №6</h1>
       </main>
       <aside className={styles.aside}>
-        <AddTask onAdd={addTask}/>
+        <AddProduct1/>
+        <AddProduct2/>
       </aside>
     </div>
   )
---
+++ frontend/src/components/UI/AddProduct1/index.js
@@ -0,0 +1,1 @@
+export { default } from './AddProduct1.jsx'
